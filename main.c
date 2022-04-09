9d00a4cd19745a328e563700e0c9a59d22c14d29
yinwuhui <12971672862@qq.com>
2022-04-09 14:47:48 +0800

    修改 demo 程序

--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 
 int main()
 {
-    char *my_table = s_table_create_0("table example", 10, 10, 8);
-
-    /*填充表格x，y处的值为 hello world!!!*/
-    s_table_valueset(my_table, 2, 2, "hello");
-    s_table_valueset(my_table, 3, 2, "world");
+    char *my_table = s_table_create_0("table example", 5, 4, 16);
 
     /*依次填充第 0 行，从第 2 格开始，1 2 3 4 5 6 。。。 分隔符为‘ ’*/
-   // s_table_valuesetxs(my_table, 0, 2, "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15", ' ');
+    s_table_valuesetxs(my_table, 0, 1, "name sex age work", ' ');
+    s_table_valuesetxs(my_table, 1, 1, "xiaoming man 22 programmer", ' ');
+    s_table_valuesetxs(my_table, 2, 1, "xiaohong woman 22 PM", ' ');
 
     /*依次填充第 0 列，从第 2 格开始，1 2 3 4 5 6 。。。 分隔符为‘ ’*/
-   // s_table_valuesetys(my_table, 0, 2, "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15", ' ');
+    s_table_valuesetys(my_table, 0, 1, "1 2 3 4 5 6", ' ');
+
+    /*填充表格x，y处的值为 hello world!!!*/
+    s_table_valueset(my_table, 3, 1, "23");
+    s_table_valueset(my_table, 4, 2, "tester");
 
     /*打印表格*/
     const char *tb_string = s_table_string_get(my_table);
